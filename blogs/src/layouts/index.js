a4493c17b3a49f7fb76c1815c0a562bf53f568d6
Lai Weng Han <jlwhoo7@gmail.com>
2017-12-02 01:23:13 +0800

    Created graphql query

--- blogs/src/layouts/index.js
+++ blogs/src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss'
 
 const Header = () => (
   <div
@@ -27,7 +27,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          SUPEROO7's PWA GUIDE
         </Link>
       </h1>
     </div>
---
+++ blogs/src/pages/12-02-2017-PWA-Table -Of-Content/index.md
@@ -0,0 +1,7 @@
+---
+path: '/table_of_content'
+title: 'Progressive Web App Table Of Content'
+---
+
+# Welcome to this web
+<iframe width="100%" height="300" src="//jsfiddle.net/superoo7/dz32xadv/embedded/" allowpaymentrequest allowfullscreen="allowfullscreen" frameborder="0"></iframe>
