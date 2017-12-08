1c30b47a6507bfe2050348488b0727022c4ce79f
Lai Weng Han <jlwhoo7@gmail.com>
2017-12-08 15:50:25 +0800

    added ga

--- blogs/gatsby-config.js
+++ blogs/gatsby-config.js
@@ -1,17 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `SUPEROO7's PWA Guide`,
+    title: `SUPEROO7's PWA Guide`
   },
   plugins: [
-      `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-sass`,
-      {
-          resolve: 'gatsby-source-filesystem',
-          options: {
-              path: `${__dirname}/src/pages`,
-              name: 'pages',
-          }
-      },
-      `gatsby-transformer-remark`,
-  ],
-}
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-110948376-1'
+      }
+    }
+  ]
+};
