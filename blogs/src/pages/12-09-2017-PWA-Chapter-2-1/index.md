5a9adba8ff2fae85368efb012aed9468a19164fe
Lai Weng Han <jlwhoo7@gmail.com>
2017-12-27 18:33:52 +0800

    added intro video

---
+++ blogs/src/pages/12-09-2017-PWA-Chapter-2-1/index.md
@@ -0,0 +1,27 @@
+---
+path: '/c2-1-intro-to-pwa'
+title: 'Introduction to Progressive Web App'
+published: true
+chapter: '2.1'
+date: '2017-12-09'
+---
+__Progressive Web App__ (PWA) is defined in wikipedia as  web applications that use the latest web technologies.
+
+My definition on PWA is more like a practise that a
+developer take to enhance User Experience in a website.
+PWA can be done slowly by adding a couple of modern
+web features into any existing web app, to further
+enhance UX.
+
+The PWA not just focusing on responsive web design,
+it enhance more on the experience
+
+
+## Features of Progressive Web App
+According to [Google](https://developers.google.com/web/progressive-web-apps/),
+ a PWA should have:
+ - Reliable
+ - Fast
+ - Engaging
+
+ ##
--- blogs/src/pages/index.js
+++ blogs/src/pages/index.js
@@ -1,52 +1,73 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import DisqusThread from '../shared/DisqusThread'
+import React from 'react';
+import Link from 'gatsby-link';
+import DisqusThread from '../shared/DisqusThread';
 
 export default class IndexPage extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-    return(
+    return (
       <div>
         <h1>🚀 Progressive Web App Series</h1>
-        <em>By Lai Weng Han</em>
-        <hr/>
+        <em>
+          By{' '}
+          <a
+            target="_blank"
+            href="https://www.wenghan.me/about-me?q=pwa%20blog"
+          >
+            Lai Weng Han
+          </a>
+        </em>
+        <p>
+          This blog is a video series on Progressive Web Apps. Read more about{' '}
+          <a href="/c2-1-intro-to-pwa">Progressive Web App (PWA)</a>
+        </p>
+        <hr />
+        <h3>Demo of Progressive Web App</h3>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/esfi7ZLibmk"
+          frameborder="0"
+          gesture="media"
+          allow="encrypted-media"
+          allowfullscreen
+        />
         <ul>
           {this.props.data.allMarkdownRemark.edges.map(post => (
-              <li key={post.node.frontmatter.chapter}><Link to={post.node.frontmatter.path}>{post.node.frontmatter.chapter} {post.node.frontmatter.title}</Link></li>
+            <li key={post.node.frontmatter.chapter}>
+              <Link to={post.node.frontmatter.path}>
+                {post.node.frontmatter.chapter} {post.node.frontmatter.title}
+              </Link>
+            </li>
           ))}
         </ul>
         <hr />
-        <DisqusThread id="Home"
-                      title="Homepage"
-                      path="/" />
-
-          
-    </div>
-    )
+        <DisqusThread id="Home" title="Homepage" path="/" />
+      </div>
+    );
   }
 }
-
-
 export const postQuery = graphql`
-query IndexQuery{
-  allMarkdownRemark(
-    limit: 10
-    filter: {frontmatter: { published: { eq: true } } }
-	sort: { fields: [frontmatter___chapter], order: ASC }
+  query IndexQuery {
+    allMarkdownRemark(
+      limit: 10
+      filter: { frontmatter: { published: { eq: true } } }
+      sort: { fields: [frontmatter___chapter], order: ASC }
     ) {
-    edges {
-      node {
-        id
-        frontmatter {
-          path
-          title
-          chapter
-          date
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+            chapter
+            date
+          }
         }
       }
     }
   }
-}`
+`;
